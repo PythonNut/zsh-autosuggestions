5eaa6690c3de34161659d92caa64d66826fe8f3f
PythonNut <PythonNut@users.noreply.github.com>
2018-09-27 13:53:53 -0700

    Tabs → spaces

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -39,32 +39,32 @@ ZSH_AUTOSUGGEST_ORIGINAL_WIDGET_PREFIX=autosuggest-orig-
 
 # Widgets that clear the suggestion
 ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(
-	history-search-forward
-	history-search-backward
-	history-beginning-search-forward
-	history-beginning-search-backward
-	history-substring-search-up
-	history-substring-search-down
-	up-line-or-history
-	down-line-or-history
-	accept-line
+    history-search-forward
+    history-search-backward
+    history-beginning-search-forward
+    history-beginning-search-backward
+    history-substring-search-up
+    history-substring-search-down
+    up-line-or-history
+    down-line-or-history
+    accept-line
 )
 
 # Widgets that accept the entire suggestion
 ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=(
-	forward-char
-	end-of-line
-	vi-forward-char
-	vi-end-of-line
+    forward-char
+    end-of-line
+    vi-forward-char
+    vi-end-of-line
 )
 
 # Widgets that accept the suggestion as far as the cursor moves
 ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(
-	forward-word
-	vi-forward-word
-	vi-forward-word-end
-	vi-forward-blank-word
-	vi-forward-blank-word-end
+    forward-word
+    vi-forward-word
+    vi-forward-word-end
+    vi-forward-blank-word
+    vi-forward-blank-word-end
 )
 
 #--------------------------------------------------------------------#
@@ -72,33 +72,33 @@ ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(
 #--------------------------------------------------------------------#
 
 _zsh_autosuggest_deprecated_warning() {
-	>&2 echo "zsh-autosuggestions: $@"
+    >&2 echo "zsh-autosuggestions: $@"
 }
 
 _zsh_autosuggest_check_deprecated_config() {
-	if [ -n "$AUTOSUGGESTION_HIGHLIGHT_COLOR" ]; then
-		_zsh_autosuggest_deprecated_warning "AUTOSUGGESTION_HIGHLIGHT_COLOR is deprecated. Use ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE instead."
-		[ -z "$ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE" ] && ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=$AUTOSUGGESTION_HIGHLIGHT_STYLE
-		unset AUTOSUGGESTION_HIGHLIGHT_STYLE
-	fi
+    if [ -n "$AUTOSUGGESTION_HIGHLIGHT_COLOR" ]; then
+        _zsh_autosuggest_deprecated_warning "AUTOSUGGESTION_HIGHLIGHT_COLOR is deprecated. Use ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE instead."
+        [ -z "$ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE" ] && ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=$AUTOSUGGESTION_HIGHLIGHT_STYLE
+        unset AUTOSUGGESTION_HIGHLIGHT_STYLE
+    fi
 
-	if [ -n "$AUTOSUGGESTION_HIGHLIGHT_CURSOR" ]; then
-		_zsh_autosuggest_deprecated_warning "AUTOSUGGESTION_HIGHLIGHT_CURSOR is deprecated."
-		unset AUTOSUGGESTION_HIGHLIGHT_CURSOR
-	fi
+    if [ -n "$AUTOSUGGESTION_HIGHLIGHT_CURSOR" ]; then
+        _zsh_autosuggest_deprecated_warning "AUTOSUGGESTION_HIGHLIGHT_CURSOR is deprecated."
+        unset AUTOSUGGESTION_HIGHLIGHT_CURSOR
+    fi
 
-	if [ -n "$AUTOSUGGESTION_ACCEPT_RIGHT_ARROW" ]; then
-		_zsh_autosuggest_deprecated_warning "AUTOSUGGESTION_ACCEPT_RIGHT_ARROW is deprecated. The right arrow now accepts the suggestion by default."
-		unset AUTOSUGGESTION_ACCEPT_RIGHT_ARROW
-	fi
+    if [ -n "$AUTOSUGGESTION_ACCEPT_RIGHT_ARROW" ]; then
+        _zsh_autosuggest_deprecated_warning "AUTOSUGGESTION_ACCEPT_RIGHT_ARROW is deprecated. The right arrow now accepts the suggestion by default."
+        unset AUTOSUGGESTION_ACCEPT_RIGHT_ARROW
+    fi
 }
 
 _zsh_autosuggest_deprecated_start_widget() {
-	_zsh_autosuggest_deprecated_warning "The autosuggest-start widget is deprecated. For more info, see the README at https://github.com/zsh-users/zsh-autosuggestions."
-	zle -D autosuggest-start
-	eval "zle-line-init() {
-		$(echo $functions[${widgets[zle-line-init]#*:}] | sed -e 's/zle autosuggest-start//g')
-	}"
+    _zsh_autosuggest_deprecated_warning "The autosuggest-start widget is deprecated. For more info, see the README at https://github.com/zsh-users/zsh-autosuggestions."
+    zle -D autosuggest-start
+    eval "zle-line-init() {
+        $(echo $functions[${widgets[zle-line-init]#*:}] | sed -e 's/zle autosuggest-start//g')
+    }"
 }
 
 zle -N autosuggest-start _zsh_autosuggest_deprecated_start_widget
@@ -109,77 +109,77 @@ zle -N autosuggest-start _zsh_autosuggest_deprecated_start_widget
 
 # Bind a single widget to an autosuggest widget, saving a reference to the original widget
 _zsh_autosuggest_bind_widget() {
-	local widget=$1
-	local autosuggest_action=$2
-	local prefix=$ZSH_AUTOSUGGEST_ORIGINAL_WIDGET_PREFIX
+    local widget=$1
+    local autosuggest_action=$2
+    local prefix=$ZSH_AUTOSUGGEST_ORIGINAL_WIDGET_PREFIX
 
-	# Save a reference to the original widget
-	case $widgets[$widget] in
-		# Already bound
-		(user:_zsh_autosuggest_(bound|orig)_*);;
+    # Save a reference to the original widget
+    case $widgets[$widget] in
+        # Already bound
+        (user:_zsh_autosuggest_(bound|orig)_*);;
 
-		# User-defined widget
-		(user:*)
-			zle -N $prefix$widget ${widgets[$widget]#*:}
-			;;
+        # User-defined widget
+        (user:*)
+            zle -N $prefix$widget ${widgets[$widget]#*:}
+            ;;
 
-		# Built-in widget
-		(builtin)
-			eval "_zsh_autosuggest_orig_$widget() { zle .$widget }"
-			zle -N $prefix$widget _zsh_autosuggest_orig_$widget
-			;;
+        # Built-in widget
+        (builtin)
+            eval "_zsh_autosuggest_orig_$widget() { zle .$widget }"
+            zle -N $prefix$widget _zsh_autosuggest_orig_$widget
+            ;;
 
-		# Completion widget
-		(completion:*)
-			eval "zle -C $prefix$widget ${${widgets[$widget]#*:}/:/ }"
-			;;
-	esac
+        # Completion widget
+        (completion:*)
+            eval "zle -C $prefix$widget ${${widgets[$widget]#*:}/:/ }"
+            ;;
+    esac
 
-	# Pass the original widget's name explicitly into the autosuggest
-	# function. Use this passed in widget name to call the original
-	# widget instead of relying on the $WIDGET variable being set
-	# correctly. $WIDGET cannot be trusted because other plugins call
-	# zle without the `-w` flag (e.g. `zle self-insert` instead of
-	# `zle self-insert -w`).
-	eval "_zsh_autosuggest_bound_$widget() {
-		_zsh_autosuggest_widget_$autosuggest_action $prefix$widget \$@
-	}"
+    # Pass the original widget's name explicitly into the autosuggest
+    # function. Use this passed in widget name to call the original
+    # widget instead of relying on the $WIDGET variable being set
+    # correctly. $WIDGET cannot be trusted because other plugins call
+    # zle without the `-w` flag (e.g. `zle self-insert` instead of
+    # `zle self-insert -w`).
+    eval "_zsh_autosuggest_bound_$widget() {
+        _zsh_autosuggest_widget_$autosuggest_action $prefix$widget \$@
+    }"
 
-	# Create the bound widget
-	zle -N $widget _zsh_autosuggest_bound_$widget
+    # Create the bound widget
+    zle -N $widget _zsh_autosuggest_bound_$widget
 }
 
 # Map all configured widgets to the right autosuggest widgets
 _zsh_autosuggest_bind_widgets() {
-	local widget;
+    local widget;
 
-	# Find every widget we might want to bind and bind it appropriately
-	for widget in ${${(f)"$(builtin zle -la)"}:#(.*|_*|orig-*|autosuggest-*|$ZSH_AUTOSUGGEST_ORIGINAL_WIDGET_PREFIX*|zle-line-*|run-help|which-command|beep|set-local-history|yank)}; do
-		if [ ${ZSH_AUTOSUGGEST_CLEAR_WIDGETS[(r)$widget]} ]; then
-			_zsh_autosuggest_bind_widget $widget clear
-		elif [ ${ZSH_AUTOSUGGEST_ACCEPT_WIDGETS[(r)$widget]} ]; then
-			_zsh_autosuggest_bind_widget $widget accept
-		elif [ ${ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS[(r)$widget]} ]; then
-			_zsh_autosuggest_bind_widget $widget partial_accept
-		else
-			# Assume any unspecified widget might modify the buffer
-			_zsh_autosuggest_bind_widget $widget modify
-		fi
-	done
+    # Find every widget we might want to bind and bind it appropriately
+    for widget in ${${(f)"$(builtin zle -la)"}:#(.*|_*|orig-*|autosuggest-*|$ZSH_AUTOSUGGEST_ORIGINAL_WIDGET_PREFIX*|zle-line-*|run-help|which-command|beep|set-local-history|yank)}; do
+        if [ ${ZSH_AUTOSUGGEST_CLEAR_WIDGETS[(r)$widget]} ]; then
+            _zsh_autosuggest_bind_widget $widget clear
+        elif [ ${ZSH_AUTOSUGGEST_ACCEPT_WIDGETS[(r)$widget]} ]; then
+            _zsh_autosuggest_bind_widget $widget accept
+        elif [ ${ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS[(r)$widget]} ]; then
+            _zsh_autosuggest_bind_widget $widget partial_accept
+        else
+            # Assume any unspecified widget might modify the buffer
+            _zsh_autosuggest_bind_widget $widget modify
+        fi
+    done
 }
 
 # Given the name of an original widget and args, invoke it, if it exists
 _zsh_autosuggest_invoke_original_widget() {
-	# Do nothing unless called with at least one arg
-	[ $# -gt 0 ] || return
+    # Do nothing unless called with at least one arg
+    [ $# -gt 0 ] || return
 
-	local original_widget_name=$1
+    local original_widget_name=$1
 
-	shift
+    shift
 
-	if [ $widgets[$original_widget_name] ]; then
-		zle $original_widget_name -- $@
-	fi
+    if [ $widgets[$original_widget_name] ]; then
+        zle $original_widget_name -- $@
+    fi
 }
 
 #--------------------------------------------------------------------#
@@ -188,20 +188,20 @@ _zsh_autosuggest_invoke_original_widget() {
 
 # If there was a highlight, remove it
 _zsh_autosuggest_highlight_reset() {
-	if [ -n "$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT" ]; then
-		region_highlight=("${(@)region_highlight:#$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT}")
-		unset _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT
-	fi
+    if [ -n "$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT" ]; then
+        region_highlight=("${(@)region_highlight:#$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT}")
+        unset _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT
+    fi
 }
 
 # If there's a suggestion, highlight it
 _zsh_autosuggest_highlight_apply() {
-	if [ $#POSTDISPLAY -gt 0 ]; then
-		_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT="$#BUFFER $(($#BUFFER + $#POSTDISPLAY)) $ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE"
-		region_highlight+=("$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT")
-	else
-		unset _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT
-	fi
+    if [ $#POSTDISPLAY -gt 0 ]; then
+        _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT="$#BUFFER $(($#BUFFER + $#POSTDISPLAY)) $ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE"
+        region_highlight+=("$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT")
+    else
+        unset _ZSH_AUTOSUGGEST_LAST_HIGHLIGHT
+    fi
 }
 
 #--------------------------------------------------------------------#
@@ -210,41 +210,41 @@ _zsh_autosuggest_highlight_apply() {
 
 # Get a suggestion from history that matches a given prefix
 _zsh_autosuggest_suggestion() {
-	setopt localoptions EXTENDED_GLOB
+    setopt localoptions EXTENDED_GLOB
 
-	# Escape special chars in the string (requires EXTENDED_GLOB)
-	local prefix="${@//(#m)[\\()\[\]|*?]/\\$MATCH}"
-	local -a recent=(${"${(f)$(cat $HISTFILE)}"#: [0-9]##:0;})
-	# Echo the first item that matches
-	echo -E ${recent[(R)$prefix*]}
+    # Escape special chars in the string (requires EXTENDED_GLOB)
+    local prefix="${@//(#m)[\\()\[\]|*?]/\\$MATCH}"
+    local -a recent=(${"${(f)$(cat $HISTFILE)}"#: [0-9]##:0;})
+    # Echo the first item that matches
+    echo -E ${recent[(R)$prefix*]}
 }
 
 _zle_synchronize_postdisplay() {
-	if [[ -n "$POSTDISPLAY_INTERNAL" && -n $BUFFER ]]; then
-		if [[ $POSTDISPLAY_INTERNAL == "$BUFFER"* ]]; then
-			POSTDISPLAY=${POSTDISPLAY_INTERNAL#$BUFFER}
-		else
-			# The POSTDISPLAY is out-of-date so recompute it
-			unset POSTDISPLAY
-			async_job suggest _zsh_autosuggest_suggestion $BUFFER
-		fi
-	else
-		unset POSTDISPLAY
-	fi
+    if [[ -n "$POSTDISPLAY_INTERNAL" && -n $BUFFER ]]; then
+        if [[ $POSTDISPLAY_INTERNAL == "$BUFFER"* ]]; then
+            POSTDISPLAY=${POSTDISPLAY_INTERNAL#$BUFFER}
+        else
+            # The POSTDISPLAY is out-of-date so recompute it
+            unset POSTDISPLAY
+            async_job suggest _zsh_autosuggest_suggestion $BUFFER
+        fi
+    else
+        unset POSTDISPLAY
+    fi
 
-	# Force the highlighting to take place immediately
-	_zsh_autosuggest_highlight_apply
-	zle -R
+    # Force the highlighting to take place immediately
+    _zsh_autosuggest_highlight_apply
+    zle -R
 }
 
 zle -N _zle_synchronize_postdisplay
 
 _zsh_autosuggest_callback() {
-	# Add the suggestion to the POSTDISPLAY proxy variable
-	# We can't modify ZLE variables in this callback, but
-	# We can through ZLE widgets.
-	POSTDISPLAY_INTERNAL=$3
-	zle _zle_synchronize_postdisplay
+    # Add the suggestion to the POSTDISPLAY proxy variable
+    # We can't modify ZLE variables in this callback, but
+    # We can through ZLE widgets.
+    POSTDISPLAY_INTERNAL=$3
+    zle _zle_synchronize_postdisplay
 }
 
 async_init
@@ -257,72 +257,72 @@ async_register_callback suggest _zsh_autosuggest_callback
 
 # Clear the suggestion
 _zsh_autosuggest_clear() {
-	# Remove the suggestion
-	unset POSTDISPLAY
+    # Remove the suggestion
+    unset POSTDISPLAY
 
-	_zsh_autosuggest_invoke_original_widget $@
+    _zsh_autosuggest_invoke_original_widget $@
 }
 
 # Modify the buffer and get a new suggestion
 _zsh_autosuggest_modify() {
-	# Original widget modifies the buffer
-	_zsh_autosuggest_invoke_original_widget $@
+    # Original widget modifies the buffer
+    _zsh_autosuggest_invoke_original_widget $@
 
-	# Get a new suggestion if the buffer is not empty after modification
-	local suggestion
-	zle _zle_synchronize_postdisplay
-	if [[ -n $BUFFER ]]; then
-		async_job suggest _zsh_autosuggest_suggestion $BUFFER
-	fi
+    # Get a new suggestion if the buffer is not empty after modification
+    local suggestion
+    zle _zle_synchronize_postdisplay
+    if [[ -n $BUFFER ]]; then
+        async_job suggest _zsh_autosuggest_suggestion $BUFFER
+    fi
 }
 
 # Accept the entire suggestion
 _zsh_autosuggest_accept() {
-	# Only accept if the cursor is at the end of the buffer
-	if (( $CURSOR == $#BUFFER )); then
-		# Add the suggestion to the buffer
-		BUFFER="$BUFFER$POSTDISPLAY"
+    # Only accept if the cursor is at the end of the buffer
+    if (( $CURSOR == $#BUFFER )); then
+        # Add the suggestion to the buffer
+        BUFFER="$BUFFER$POSTDISPLAY"
 
-		# Remove the suggestion
-		unset POSTDISPLAY
+        # Remove the suggestion
+        unset POSTDISPLAY
 
-		# Move the cursor to the end of the buffer
-		CURSOR=${#BUFFER}
-	fi
+        # Move the cursor to the end of the buffer
+        CURSOR=${#BUFFER}
+    fi
 
-	_zsh_autosuggest_invoke_original_widget $@
+    _zsh_autosuggest_invoke_original_widget $@
 }
 
 # Partially accept the suggestion
 _zsh_autosuggest_partial_accept() {
-	# Save the contents of the buffer so we can restore later if needed
-	local original_buffer=$BUFFER
+    # Save the contents of the buffer so we can restore later if needed
+    local original_buffer=$BUFFER
 
-	# Temporarily accept the suggestion.
-	BUFFER="$BUFFER$POSTDISPLAY"
+    # Temporarily accept the suggestion.
+    BUFFER="$BUFFER$POSTDISPLAY"
 
-	# Original widget moves the cursor
-	_zsh_autosuggest_invoke_original_widget $@
+    # Original widget moves the cursor
+    _zsh_autosuggest_invoke_original_widget $@
 
-	# If we've moved past the end of the original buffer
-	if (( $CURSOR > $#original_buffer )); then
-		# Set POSTDISPLAY to text right of the cursor
-		POSTDISPLAY=$RBUFFER
+    # If we've moved past the end of the original buffer
+    if (( $CURSOR > $#original_buffer )); then
+        # Set POSTDISPLAY to text right of the cursor
+        POSTDISPLAY=$RBUFFER
 
-		# Clip the buffer at the cursor
-		BUFFER=$LBUFFER
-	else
-		# Restore the original buffer
-		BUFFER=$original_buffer
-	fi
+        # Clip the buffer at the cursor
+        BUFFER=$LBUFFER
+    else
+        # Restore the original buffer
+        BUFFER=$original_buffer
+    fi
 }
 
 for action in clear modify accept partial_accept; do
-	eval "_zsh_autosuggest_widget_$action() {
-		_zsh_autosuggest_highlight_reset
-		_zsh_autosuggest_$action \$@
-		_zsh_autosuggest_highlight_apply
-	}"
+    eval "_zsh_autosuggest_widget_$action() {
+        _zsh_autosuggest_highlight_reset
+        _zsh_autosuggest_$action \$@
+        _zsh_autosuggest_highlight_apply
+    }"
 done
 
 zle -N autosuggest-accept _zsh_autosuggest_widget_accept
@@ -334,8 +334,8 @@ zle -N autosuggest-clear _zsh_autosuggest_widget_clear
 
 # Start the autosuggestion widgets
 _zsh_autosuggest_start() {
-	_zsh_autosuggest_check_deprecated_config
-	_zsh_autosuggest_bind_widgets
+    _zsh_autosuggest_check_deprecated_config
+    _zsh_autosuggest_bind_widgets
 }
 
 autoload -Uz add-zsh-hook
